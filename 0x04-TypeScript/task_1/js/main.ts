4690bd2c3e1e3a031a239e2fe2216565e0938cc3
Collinsgra <kipkemoicollins941@gmail.com>
2024-06-07 22:48:12 +0000

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,14 +19,14 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-const teacher_two: Teacher = {
+const teacher_one: Teacher = {
     firstName: 'William',
     fullTimeEmployee: true,
     lastName: 'Ruto',
     location: 'Kenya'
 };
 
-const teacher_one: Teacher = {
+const teacher_two: Teacher = {
     firstName: 'Jane',
     fullTimeEmployee: false,
     lastName: 'Vicky',
@@ -37,22 +37,58 @@ const teacher_one: Teacher = {
 const teacher3: Teacher = {
     firstName: 'Jack',
     fullTimeEmployee: false,
-    lastName: 'Mudryk',
-    location: 'Ukraine',
+    lastName: 'Smith',
+    location: 'Canada',
     contract: false
 };
 
 const director_one: Directors = {
     firstName: 'John',
-    lastName: 'Smith',
-    location: 'Canada',
+    lastName: 'Mudryk',
+    location: 'Ukraine',
     fullTimeEmployee: true,
     numberOfReports: 17
 };
 
-console.log(teacher_two);
 console.log(teacher_one);
+console.log(teacher_two);
 console.log(teacher3);
 console.log(director_one);
 
 console.log(printTeacher("William", "Ruto"));  // Output: W. Ruto
+
+
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+
+const student = new StudentClass('William', 'Ruto');
+console.log(student.displayName()); // Output: William
+console.log(student.workOnHomework()); // Output: Currently working
