665e1d4e8a4404b9b7d5dc11d2e763c1991362a3
Collinsgra <kipkemoicollins941@gmail.com>
2024-06-07 22:14:39 +0000

    Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
     [propName: string]: any;
 }
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
 const teacher_one: Teacher = {
     firstName: 'William',
     fullTimeEmployee: true,
@@ -17,12 +21,12 @@ const teacher_one: Teacher = {
 const teacher_two: Teacher = {
     firstName: 'Jane',
     fullTimeEmployee: false,
-    lastName: 'vicky',
+    lastName: 'Vicky',
     location: 'USA',
     yearsOfExperience: 10
 };
 
-const teacher_3: Teacher = {
+const teacher3: Teacher = {
     firstName: 'Jack',
     fullTimeEmployee: false,
     lastName: 'Mudryk',
@@ -30,6 +34,15 @@ const teacher_3: Teacher = {
     contract: false
 };
 
+const director_one: Directors = {
+    firstName: 'John',
+    lastName: 'Smith',
+    location: 'Canada',
+    fullTimeEmployee: true,
+    numberOfReports: 17
+};
+
 console.log(teacher_one);
 console.log(teacher_two);
-console.log(teacher_3);
+console.log(teacher3);
+console.log(director_one);
